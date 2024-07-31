8b07314ce80659098afc4e478ae4d309c4ef789f
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-07-31 19:37:09 +0530

    ccc

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -7,29 +7,8 @@ import './Login.css'; // Make sure to create and import this CSS file
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <div className="login-box card" style={{backgroundImage:BlueBanner}}>
-        <div className="logo-section">
-          <img src={logoitdose} alt="itdoseinfosystem logo" className="logo-image" />
-          <h1 className="logo-text">ELABPRO</h1>
-        </div>
-        <div className="login-content">
-          <div className="login-field">
-            <label className="login-label">Username</label>
-            <InputText id="username" type="text" className="login-input" />
-          </div>
-          <div className="login-field">
-            <label className="login-label">Password</label>
-            <InputText id="password" type="password" className="login-input" />
-          </div>
-          <Button
-            label="Login"
-            icon="pi pi-user"
-            className="login-button"
-          ></Button>
-        </div>
-      </div>
-    </div>
+  <>
+  </>
   );
 };
 
