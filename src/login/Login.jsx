35284cc643fd69df9895d868d772cc6ea13eafe8
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-07-31 19:37:31 +0530

    qq

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import logoitdose from "../assets/image/logoitdose.png"
-import BlueBanner from "../assets/image/BlueBanner.jpg"
-import './Login.css'; // Make sure to create and import this CSS file
 
 const Login = () => {
   return (
