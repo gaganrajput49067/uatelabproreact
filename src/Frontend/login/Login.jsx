fedd1ee2e7d96d2f25ef3410f2fba720a8be66de
joinprakhar <pandeylive@gmail.com>
2024-08-01 16:01:01 +0530

    chnages

--- src/Frontend/login/Login.jsx
+++ src/Frontend/login/Login.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import "../../zStyles/login.css";
+import logoitdose from "../../assets/image/logoitdose.png";
+import { Link } from "react-router-dom";
+import Input from "../../components/CommonComponent/Input";
+import { useTranslation } from "react-i18next";
 
 const Login = () => {
-  return <>This is login Page</>;
+  const [t] = useTranslation();
+  return (
+    <div className="main-login-outer-Container">
+      <div className="main-login-inner-container">
+        <div className="login-form-container">
+          <div className="login-form">
+            <Link to="/">
+              <img className="logoStyle" src={logoitdose} />
+            </Link>
+            <h5 className="logo-title"> Sign in to start your session</h5>
+
+            <div className="main-login-input">
+              <div className="icondiv">
+                <i className="fas fa-user-alt" />
+              </div>
+              <div className="maindiv">
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="text"
+                  name="userName"
+                  lable={t("Username")}
+                  placeholder=" "
+                  // value={values?.userName}
+                  // onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="main-login-input">
+              <div className="icondiv">
+                <i className="fas fa-lock" />
+              </div>
+              <div className="maindiv">
+                <Input
+                  type="text"
+                  className="form-control"
+                  id="text"
+                  name="password"
+                  lable={t("Password")}
+                  placeholder=" "
+                  // value={values?.userName}
+                  // onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="main-login-button">
+              <button
+                className=" btn btn-sm btn-primary btn-block login-button"
+                // onClick={handleSubmit}
+              >
+                Login
+              </button>
+              <span className="forgetpassword"> Forget Password</span>
+            </div>
+          </div>
+        </div>
+        <div className="login-style-container">efwf</div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/CommonComponent/Input.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Tooltip } from "primereact/tooltip";
+
+function Input({
+  type,
+  name,
+  className,
+  respclass,
+  id,
+  placeholder,
+  lable,
+  value,
+  onKeyDown,
+  required,
+  display,
+  onChange,
+  disabled,
+  readOnly,
+  defaultValue,
+  onBlur,
+  inputRef,
+  removeFormGroupClass,
+  onInput,
+  max,
+  min,
+  showTooltipCount,
+  tabIndex,
+}) {
+  const [t] = useTranslation();
+
+  return (
+    <>
+      <div className={`${respclass}  custominputbox`}>
+        {/* <div className={!isFromGroup ? "" : "form-group"}> */}
+        <div className={removeFormGroupClass ? "" : "form-group"}>
+          <input
+            type={type}
+            className={className}
+            id={id}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onKeyDown={onKeyDown}
+            onChange={onChange}
+            autoComplete="off"
+            step={type === "number" ? "0.01" : ""}
+            required={required}
+            ref={inputRef}
+            onBlur={onBlur}
+            max={max}
+            min={min}
+            style={{ textAlign: display ?? "left" }}
+            onInput={onInput}
+            disabled={disabled ? disabled : false}
+            tabIndex={tabIndex ? tabIndex : "-1"}
+            readOnly={readOnly}
+          />
+          <label htmlFor={id} className="lable truncate">
+            {lable}
+          </label>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Input;
