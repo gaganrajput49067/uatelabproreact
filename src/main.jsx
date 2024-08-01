ea94e0a658b6b393c0c2fabdbe8ea5a28165d9ce
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-08-01 11:01:43 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { PrimeReactProvider } from 'primereact/api';
 import 'primeflex/primeflex.css';  
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PrimeReactProvider>
     {/* <Provider store={store}> */}
