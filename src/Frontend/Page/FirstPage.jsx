c8105ba567e3b1e4fbf671331cf587ac2025fc37
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-08-05 17:45:40 +0530

    cc

--- src/Frontend/Page/FirstPage.jsx
+++ src/Frontend/Page/FirstPage.jsx
@@ -41,117 +41,125 @@ const FirstPage = () => {
     console.log(e);
   };
   return (
-    <div className="container">
-      <div className="card border-success my-3">
-        <div className="card-header bg-success text-white">
-          <h3 className="card-title">Route Master</h3>
-        </div>
-        <div className="card-body">
-          <div
-            className="divResult boottable table-responsive"
-            id="no-more-tables"
-          >
-            <table className="table table-bordered table-hover table-striped tbRecord">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {dummyData.map((item) => (
-                  <tr key={item.id}>
-                    <td data-title="ID">{item.id}</td>
-                    <td data-title="Name">{item.name}</td>
-                    <td data-title="Email">{item.email}</td>
-                    <td data-title="Status">{item.status}</td>
+    <>
+      <div className="container-fluid">
+        <div className="card border-success my-3">
+          <div className="card-header bg-success text-white">
+            <h3 className="card-title">Route Master</h3>
+          </div>
+          <div className="card-body">
+            <div
+              className="divResult boottable table-responsive"
+              id="no-more-tables"
+            >
+              <table className="table table-bordered table-hover table-striped tbRecord">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Status</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {dummyData.map((item) => (
+                    <tr key={item.id}>
+                      <td data-title="ID">{item.id}</td>
+                      <td data-title="Name">{item.name}</td>
+                      <td data-title="Email">{item.email}</td>
+                      <td data-title="Status">{item.status}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-        <div className="card-body">
-          <h4 className="card-title">Add New Entry</h4>
-          <form>
-            <div className="row">
-              <div className="col-sm-3">
-                <Input
-                  type="text"
-                  id="username"
-                  className="form-control required-fields"
-                  name="username"
-                  lable="Username"
-                  placeholder=" "
-                />
+      </div>
+
+      <div className="container-fluid ">
+        <div className="card border-success">
+          {" "}
+          <div className="card-body">
+            <h4 className="card-title">Add New Entry</h4>
+            <form>
+              <div className="row">
+                <div className="col-sm-3">
+                  <Input
+                    type="text"
+                    id="username"
+                    className="form-control required-fields"
+                    name="username"
+                    lable="Username"
+                    placeholder=" "
+                  />
+                </div>
+                <div className="col-sm-3">
+                  <SelectBox
+                    className="form-control form-control-sm"
+                    placeholder=" "
+                    id="SelectBox2"
+                    lable="SelectBox"
+                    name="SelectBox"
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-sm-3">
+                  <DatePicker
+                    className="custom-calendar"
+                    id="DOB"
+                    name="DOB"
+                    lable={"DOB"}
+                    value={new Date()}
+                  />
+                </div>
+                <div className="col-sm-3">
+                  <Input
+                    type="time"
+                    id="time"
+                    className="form-control required-fields"
+                    name="time"
+                    lable="time"
+                    placeholder=" "
+                  />
+                </div>
               </div>
-              <div className="col-sm-3">
-                <SelectBox
-                  className="form-control form-control-sm"
-                  placeholder=" "
-                  id="SelectBox2"
-                  lable="SelectBox"
-                  name="SelectBox"
-                  onChange={handleChange}
-                />
+              <div className="row">
+                <div className="col-sm-3">
+                  <BrowseButton accept="image/*" />
+                </div>
+                <div className="col-sm-3">
+                  <SelectBox
+                    inputId="SelectBox"
+                    placeholder="SelectBox"
+                    id="SelectBox"
+                    name="SelectBox"
+                    searchable={true}
+                  />
+                </div>
+                <div className="col-sm-3">
+                  <MultiSelectComp
+                    options={options}
+                    placeholder="Select Options"
+                    style={{ width: "15rem" }}
+                  />
+                </div>
+                <div className="col-sm-3">
+                  <Input
+                    type="time"
+                    id="time"
+                    className="form-control required-fields"
+                    name="time"
+                    lable="time"
+                    placeholder=" "
+                  />
+                </div>
               </div>
-              <div className="col-sm-3">
-                <DatePicker
-                  className="custom-calendar"
-                  id="DOB"
-                  name="DOB"
-                  lable={"DOB"}
-                  value={new Date()}
-                />
-              </div>
-              <div className="col-sm-3">
-                <Input
-                  type="time"
-                  id="time"
-                  className="form-control required-fields"
-                  name="time"
-                  lable="time"
-                  placeholder=" "
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-3">
-                <BrowseButton accept="image/*" />
-              </div>
-              <div className="col-sm-3">
-                <SelectBox
-                  inputId="SelectBox"
-                  placeholder="SelectBox"
-                  id="SelectBox"
-                  name="SelectBox"
-                  searchable={true}
-                />
-              </div>
-              <div className="col-sm-3">
-                <MultiSelectComp
-                  options={options}
-                  placeholder="Select Options"
-                  style={{ width: "15rem" }}
-                />
-              </div>
-              <div className="col-sm-3">
-                <Input
-                  type="time"
-                  id="time"
-                  className="form-control required-fields"
-                  name="time"
-                  lable="time"
-                  placeholder=" "
-                />
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
