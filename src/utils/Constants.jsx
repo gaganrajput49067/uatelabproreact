7bba6e8d18933959874d0446e718183b8255f7d8
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-08-13 11:35:25 +0530

    12234

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -94,55 +94,77 @@ export const DISCOUNT_TYPE = [
   { label: "Discount Type Wise", value: 2 },
 ];
 export const SampleStatusSearch = [
-    {
-      label: "Sample Not Collected",
-      value: 1,
-      status: true,
-    },
-    {
-      label: "Collected",
-      value: 2,
-      status: true,
-    },
-    {
-      label: "Received",
-      value: 3,
-      status: true,
-    },
-    {
-      label: "Rejected",
-      value: 4,
-      status: true,
-    },
-  ];
-  export const SearchBy = [
-    { label: "Select", value: "" },
-    { label: "BarcodeNo", value: "BarcodeNo" },
-    { label: "Mobile", value: "Mobile" },
-    { label: "PatientCode", value: "PatientCode" },
-    { label: "PatientName", value: "PatientName" },
-    { label: "VisitNo", value: "VisitNo" },
-  ];export const NoOfPricks = [
-    { label: "Select", value: "" },
-    { label: "1", value: 1 },
-    { label: "2", value: 2 },
-    { label: "3", value: 3 },
-    { label: "4", value: 4 },
-    { label: "5", value: 5 },
-    { label: "6", value: 6 },
-    { label: "7", value: 7 },
-    { label: "8", value: 8 },
-    { label: "9", value: 9 },
-    { label: "10", value: 10 },
-  
-  ];export const SampleSource = [
-    {
-      label: "Left Arm",
-      value: "Left Arm",
-    },
-    {
-      label: "Right Arm",
-      value: "Right Arm",
-    },
-  ];
-  
+  {
+    label: "Sample Not Collected",
+    value: 1,
+    status: true,
+  },
+  {
+    label: "Collected",
+    value: 2,
+    status: true,
+  },
+  {
+    label: "Received",
+    value: 3,
+    status: true,
+  },
+  {
+    label: "Rejected",
+    value: 4,
+    status: true,
+  },
+];
+export const SearchBy = [
+  { label: "Select", value: "" },
+  { label: "BarcodeNo", value: "BarcodeNo" },
+  { label: "Mobile", value: "Mobile" },
+  { label: "PatientCode", value: "PatientCode" },
+  { label: "PatientName", value: "PatientName" },
+  { label: "VisitNo", value: "VisitNo" },
+];
+export const NoOfPricks = [
+  { label: "Select", value: "" },
+  { label: "1", value: 1 },
+  { label: "2", value: 2 },
+  { label: "3", value: 3 },
+  { label: "4", value: 4 },
+  { label: "5", value: 5 },
+  { label: "6", value: 6 },
+  { label: "7", value: 7 },
+  { label: "8", value: 8 },
+  { label: "9", value: 9 },
+  { label: "10", value: 10 },
+];
+export const SampleSource = [
+  {
+    label: "Left Arm",
+    value: "Left Arm",
+  },
+  {
+    label: "Right Arm",
+    value: "Right Arm",
+  },
+];
+
+export const DateTypeSearch = [
+  { label: "Registration Date", value: "Date" },
+  { label: "Sample Collected Date", value: "SampleCollectionDate" },
+  { label: "Department Receive Date", value: "DepartmentReceiveDate" },
+];
+export const SampleStatus = [
+  // { label: " Serach", value: ""},
+  { label: "Not Collected", value: "1", status: true },
+  { label: "Collected", value: "2", status: true },
+  { label: "Receive", value: "3", status: true },
+  { label: "Rejected", value: "4", status: true },
+  { label: "Result Done", value: "10", status: true },
+  { label: "Approved", value: "5", status: true },
+  { label: "Hold", value: "11", status: true },
+  { label: "Re-Run", value: "14", status: true },
+  { label: "Mac Data", value: "13", status: true },
+  { label: "Dispatched", value: "15", status: true },
+  { label: "Printed", value: "6", status: true },
+  { label: "OutSource", value: "18", status: true },
+  { label: "All", value: "", status: true },
+];
