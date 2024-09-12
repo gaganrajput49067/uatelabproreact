0a6207c992be2b6807cce2970ef8e97c6f70313a
joinprakhar <pandeylive@gmail.com>
2024-09-12 16:30:53 +0530

    kfdhf

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,19 +3,21 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <div className="footer-content">
-        <div className="footer-left">
-          <strong>
-            Copyright © {new Date().getFullYear()}{" "}
-            <a href="http://uat.elabpro.in" target="_blank" rel="noopener noreferrer">
-              <b>eLabPro.IN</b>
-            </a>
-            . All rights reserved.
-          </strong>
-        </div>
-        <div className="footer-right">
-          <b>Version</b> 1.0.0
-        </div>
+      <div className="footer-left">
+        <strong>
+          Copyright © {new Date().getFullYear()}{" "}
+          <a
+            href="http://uat.elabpro.in"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b>eLabPro.IN</b>
+          </a>
+          &nbsp; | &nbsp;All rights reserved.
+        </strong>
+      </div>
+      <div className="footer-right">
+        <b>Version</b> 1.0.0
       </div>
     </footer>
   );
