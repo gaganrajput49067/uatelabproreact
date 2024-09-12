55360d0690924446a2d72ee49c7f0184383a25c8
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-09-12 12:01:12 +0530

    233

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import logoitdose from "../../assets/image/logoitdose.png"
 const Loading = () => {
   return (
     <div className="loading-overlay">
+       <img src={logoitdose} alt="itdose Logo" className="itdose-logo" />
       <div className="loadingio-spinner-spinner-nq4q5u6dq7r">
         <div className="ldio-x2uulkbinbj">
           {[...Array(21)].map((_, index) => (
