5414b6855e1f9ef98006000e7b613d69ce4ecab8
gaganrajput49067 <gaganrajput49067@gmail.com>
2024-09-13 19:29:56 +0530

    123

--- src/Frontend/Laboratory/ReceiptReprint.jsx
+++ src/Frontend/Laboratory/ReceiptReprint.jsx
@@ -432,14 +432,8 @@ const ReceiptReprint = () => {
               className="col-md-1"
             >
               <button
-                style={{
-                  height: "14px",
-                  width: "16px",
-                  backgroundColor: "#00FA9A",
-                  marginRight: "3px",
-                  border: "1px solid",
-                  borderRadius: "2px",
-                }}
+              className="statusConfirmed"
+              style={{ backgroundColor: "#00FA9A" }}
               ></button>
               <label className="reprintLable" style={{ cursor: "pointer" }}>
                 {"Full Paid"}
@@ -452,14 +446,9 @@ const ReceiptReprint = () => {
               className="col-md-1"
             >
               <button
-                style={{
-                  height: "14px",
-                  width: "16px",
-                  backgroundColor: "#F6A9D1",
-                  marginRight: "3px",
-                  border: "1px solid",
-                  borderRadius: "2px",
-                }}
+               
+                className="statusConfirmed"
+                style={{ backgroundColor: "#F6A9D1" }}
               ></button>
               <label className="reprintLable" style={{ cursor: "pointer" }}>
                 {"Partial Paid"}
@@ -472,14 +461,9 @@ const ReceiptReprint = () => {
               className="col-md-1"
             >
               <button
-                style={{
-                  height: "14px",
-                  width: "16px",
-                  backgroundColor: "#FF457C",
-                  marginRight: "3px",
-                  border: "1px solid",
-                  borderRadius: "2px",
-                }}
+               
+                className="statusConfirmed"
+                style={{ backgroundColor: "#FF457C" }}
               ></button>
               <label className="reprintLable" style={{ cursor: "pointer" }}>
                 {"Fully Unpaid"}
@@ -492,14 +476,9 @@ const ReceiptReprint = () => {
               className="col-md-1"
             >
               <button
-                style={{
-                  height: "14px",
-                  width: "16px",
-                  backgroundColor: "#b3cdb3",
-                  marginRight: "3px",
-                  border: "1px solid",
-                  borderRadius: "2px",
-                }}
+              
+                className="statusConfirmed"
+                style={{ backgroundColor: "#b3cdb3" }}
               ></button>
               <label className="reprintLable" style={{ cursor: "pointer" }}>
                 {"Credit"}
@@ -513,14 +492,9 @@ const ReceiptReprint = () => {
               className="col-md-1"
             >
               <button
-                style={{
-                  height: "14px",
-                  width: "16px",
-                  backgroundColor: "#6699ff",
-                  marginRight: "3px",
-                  border: "1px solid",
-                  borderRadius: "2px",
-                }}
+                
+                className="statusConfirmed"
+                style={{ backgroundColor: "#6699ff" }}
               ></button>
               <label className="reprintLable" style={{ cursor: "pointer" }}>
                 {"Full Refund"}
